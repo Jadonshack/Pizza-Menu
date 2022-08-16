79954d3bb9e9bb98c85be75057d64f32fd7c8078
Jadon Shackelford <shackelfordjadon@gmail.com>
2022-08-16 14:53:41 -0700

    First Save

---
+++ script.js
@@ -0,0 +1,33 @@
+// Global variables
+var crust = document.getElementById('crust');
+var sauce = document.getElementById('sauce');
+// toppings array
+var toppings = [];
+// event listener
+var formButton = document.getElementById('btn');
+formButton.addEventListener("click", function(event){
+event.preventDefault();
+var top1 = document.getElementById('top1').value;
+var top2 = document.getElementById('top2').value;
+var top3 = document.getElementById('top3').value;
+
+});
+
+function calculateTotal(toppingArray) {  
+    let total = 0; 
+    let toppingCost = 2.50; 
+    let baseCost = 5.50; // cost of crust and sauce
+
+    // order string concatenation
+    
+
+    let toppingString = "Toppings: ";
+
+    // For Loop
+
+    // total = baseCost + cost of all toppings
+
+    // set DOM total += total   
+    // use DOM: = orderString   
+    // use DOM: = toppingString 
+}
